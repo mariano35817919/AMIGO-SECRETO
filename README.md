e04d7611e75881f4d2111d9a29d79149c0bca815
Mariano Martinez <marianoagustin27@gmail.com>
2025-09-21 20:22:33 -0300

    READ ME FINALIZADO

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AMIGO-SECRETO
 Aplicación web para agregar nombres de amigos y realizar u sorteo aleatorio del "amigo secreto"
+
+# ESTADO DEL PROYECTO : FINALIZADO
+
+## Descripción:
+Esta aplicación permite :
+- Agregar nombres de participantes.
+- Mostrar la lista de nombres ingresados en tiempo real.
+- Sortear un amigo de manera aleatoria y mostrar el resultado.
+
+El objetivo es **practicar JS , DOM y la manipulacion de arrays** 
+
+---
+
+## FUNCIONALIDADES:
+- Función agregarAmigo() : permite al usuario agregar un nombre para ingresar a la lista.
+- Función mostrarLista() : muestra en pantalla todos los nombres que se van agregando . 
+- Función sortearAmigo() : selecciona aleatoriamente un amigo y lo muestra como resultado.
+
+---
+
+## TECNOLOGÍAS UTILIZADAS : 
+- HTML.
+- JS (DOM, ARRAYS , EVENTOS).
+- GIT Y GITHUB.
+
+## Acceso al proyecto
+
+1- Clonar el repositorio :
+
+```bash
+git clone aca voy a escribir la url de mi proyecto.
+
--- app.js
+++ app.js
@@ -1,1 +1,99 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//const { createElement } = require("react");
+
+
+
+let amigos = [];
+
+
+
+function agregarAmigo() {
+    
+    //en esta parte capturo el valor del input 
+    const amigoAgregado = document.getElementById("amigo");
+    
+    const nombre = amigoAgregado.value 
+
+    //validamos que no este vacio
+if (nombre == "") {
+    alert ("Ingresa un nombre por favor ");
+    return;
+}
+
+
+        console.log(nombre );
+
+    //agrego el nombre al array
+    amigos.push(nombre);
+
+    //limpio el campo 
+    amigoAgregado.value = ""
+
+
+
+console.log(amigos);
+
+mostrarLista();
+
+}
+
+
+function mostrarLista() {
+
+    //obtenemos el elemento donde se van a mostrar los nobres que agregamos 
+    const lista = document.getElementById("listaAmigos");
+
+    //limpiamos lista antes de actializarla
+    lista.innerHTML="";
+
+    //vamos a iterar sobre la lista
+    for (let i = 0; i < amigos.length; i++) {
+
+        //creamos elemento  para cada amigo 
+        const friend =document.createElement("li");
+
+        friend.textContent= amigos[i];
+
+        
+        // agrego el <li> a la lista
+        lista.appendChild(friend);
+
+        
+    }
+
+}
+
+function sortearAmigo() {
+    
+    //1.validamos que haya amigos disponibles
+if (amigos.length === 0) {
+    alert("No escribiste ningun nombre")
+    return;
+}
+
+//2-Generamos indice aleatorio
+
+const indiceAleatorio = Math.floor(Math.random()* amigos.length);
+
+//3 Obtenemos el nmbre sorteado:
+
+const amigoSorteado = amigos[indiceAleatorio];
+
+//4 Ahora quiero mostrar el resultado en pantalla
+
+const resultado = document.getElementById("resultado")
+
+resultado.innerHTML=`<li>${amigoSorteado}</li> `; 
+
+
+
+}
+
+
+
+
+
+
+
+
